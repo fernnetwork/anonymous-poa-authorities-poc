c502541d5de524bc71b3b43fa2967c4581b77a24
Jimmy Chen <jchen.tc@gmail.com>
2018-09-11 22:43:50 +0100

    Add check for linkability.

--- truffle/contracts/AnonymousIdentityRegistry.sol
+++ truffle/contracts/AnonymousIdentityRegistry.sol
@@ -3,9 +3,10 @@ pragma solidity ^0.4.24;
 import './UAOSRing.sol';
 
 contract AnonymousIdentityRegistry {
-  /// key = listId
-  mapping(string => uint256[20]) pkeys;
-  mapping(string => address[]) anonymousIds;
+
+  mapping(string => uint256[20]) listIdToPubKeys;
+  mapping(string => address[]) listIdToAnonymousIds;
+  mapping(string => mapping(bytes => bool)) listIdToTags;
   
   address owner;
 
@@ -22,7 +23,7 @@ contract AnonymousIdentityRegistry {
   }
 
   function createList(string _listId, uint256[20] _pkeys) public onlyOwner {
-    pkeys[_listId] = _pkeys;
+    listIdToPubKeys[_listId] = _pkeys;
     emit ListCreated(_listId);
   }
 
@@ -30,29 +31,34 @@ contract AnonymousIdentityRegistry {
       string _listId, 
       address _anonymousId,
       // ring signature parameters
-      uint256[20] pubkeys,
-      uint256[2] tag,
-      uint256[10] tees,
-      uint256 seed
-    ) public returns (bool success) {
+      uint256[20] _pubkeys,
+      uint256[2] _tag,
+      uint256[10] _tees,
+      uint256 _seed
+    ) 
+    public returns (bool success) {
     // verify ring signature
-    bool result = UAOSRing.verify(
-      pubkeys,
-      tag,
-      tees,
-      seed,
+    require(UAOSRing.verify(
+      _pubkeys,
+      _tag,
+      _tees,
+      _seed,
       uint256(sha256(_listId))
-    );
-    require(result, 'Invalid signature');
+    ), 'Invalid signature');
 
-    address[] storage anonymousIdsOfList = anonymousIds[_listId];
-    anonymousIdsOfList.push(_anonymousId);
+    // verify tag uniqueness
+    bytes memory tagBytes = abi.encodePacked(_tag);
+    require(listIdToTags[_listId][tagBytes] == false, "Duplicate signature");
+    listIdToTags[_listId][tagBytes] = true;
+
+    address[] storage anonymousIds = listIdToAnonymousIds[_listId];
+    anonymousIds.push(_anonymousId);
     emit ListItemAdded(_listId, _anonymousId);
     
     return true;
   }
 
   function getList(string _listId) public view returns (address[]) {
-    return anonymousIds[_listId];
+    return listIdToAnonymousIds[_listId];
   }
 }
--- truffle/test/AnonymousIdentityRegistry.js
+++ truffle/test/AnonymousIdentityRegistry.js
@@ -4,6 +4,7 @@ const chaiAsPromised = require('chai-as-promised')
 const expect = chai.expect
 chai.use(chaiAsPromised)
 
+// ring signature for message '0xd3fd354067184687956bc8618a26e335'
 const signature = require('./data/signature.json')
 const AnonymousIdentityRegistry = artifacts.require('./AnonymousIdentityRegistry.sol')
 
@@ -28,7 +29,24 @@ contract('AnonymousIdentityRegistry', accounts => {
     expect(args.listId).to.equal(listId)
   })
 
-  it('should accpet new entry to list when valid signature provided', async () => {
+  it('should reject new entry to list when invalid signature provided', async () => {
+    const { pubKeys, tag, tees, cees } = signature
+
+    try {
+      await contract.addToList(
+        'unsigned message',
+        entry,
+        flat(pubKeys),
+        tag,
+        tees,
+        cees
+      )
+    } catch (err) {
+      expect(err.reason).to.equal('Invalid signature')
+    }
+  })
+
+  it('should accept new entry to list when valid signature provided', async () => {
     const { pubKeys, tag, tees, cees } = signature
     const { logs } = await contract.addToList(
       listId,
@@ -44,6 +62,23 @@ contract('AnonymousIdentityRegistry', accounts => {
     expect(event).to.equal('ListItemAdded')
     expect(args.listId).to.equal(listId)
     expect(args.anonymousId).to.equal(entry)
+  })
+
+  it('should reject new entry to list when a duplicate signature is provided', async () => {
+    const { pubKeys, tag, tees, cees } = signature
+
+    try {
+      await contract.addToList(
+        listId,
+        '0x6d480772b57e91f1c4e1cc196df88896d27ed327',
+        flat(pubKeys),
+        tag,
+        tees,
+        cees
+      )
+    } catch (err) {
+      expect(err.reason).to.equal('Duplicate signature')
+    }
   })
 
   it('should return all entries for the list', async () => {
