d50680f3fea5c4e64b088b9eb00a3270a012f99b
Jimmy Chen <jchen.tc@gmail.com>
2018-09-13 22:12:58 +0100

    Add duplicate identity, duplicate list, and list checks

--- truffle/contracts/AnonymousIdentityRegistry.sol
+++ truffle/contracts/AnonymousIdentityRegistry.sol
@@ -3,27 +3,34 @@ pragma solidity ^0.4.24;
 import './UAOSRing.sol';
 
 contract AnonymousIdentityRegistry {
+  /// mapping of all list ids
+  mapping(string => bool) listIds;
+
   /// mapping of list ids to public keys
   mapping(string => uint256[20]) listIdToPubKeys;
+
+  /// mapping of list ids to ring signature tags
+  mapping(string => mapping(bytes => bool)) listIdToTags;
 
   /// mapping of list ids to anonymous identities
   mapping(string => address[]) listIdToAnonymousIds;
 
-  /// mapping of list ids to ring signature tags
-  mapping(string => mapping(bytes => bool)) listIdToTags;
-  
+  /// mapping of all anonymous identities
+  mapping(address => bool) anonmyousIdentitiesMap;
+
   /// owner of this registry
   address owner;
 
   event ListCreated(string listId);
   event ListItemAdded(string listId, address anonymousId);
+  event ListFilled(string listId);
 
   constructor() public {
     owner = msg.sender;
   }
 
   modifier onlyOwner() {
-    require(owner == msg.sender);
+    require(owner == msg.sender, 'Unauthorized operation');
     _;
   }
 
@@ -34,7 +41,8 @@ contract AnonymousIdentityRegistry {
    * @param _pkeys authorized public keys for contributing to the list
    */
   function createList(string _listId, uint256[20] _pkeys) public onlyOwner {
-    require(listIdToPubKeys[_listId][0] == 0, "List already exists");
+    require(listIds[_listId] == false, 'List already exists');
+    listIds[_listId] = true;
     listIdToPubKeys[_listId] = _pkeys;
     emit ListCreated(_listId);
   }
@@ -44,7 +52,6 @@ contract AnonymousIdentityRegistry {
    *  An unique and valid linkable ring signature must be provided.
    * @param _listId list id to add to.
    * @param _anonymousId entry to add to list.
-   * @param _pubkeys authorised list of 10 public keys (every pair represents a public key).
    * @param _tag tag for testing linkability. Must be unique for the same listId to prevent double entry.
    * @param _tees part of the ring signature, needs further documentation.
    * @param _seed part of the ring signature, needs further documentation.
@@ -52,21 +59,23 @@ contract AnonymousIdentityRegistry {
   function addToList(
       string _listId, 
       address _anonymousId,
-      // ring signature parameters
-      uint256[20] _pubkeys,
       uint256[2] _tag,
       uint256[10] _tees,
       uint256 _seed
-    ) 
-    public returns (bool success) {
+    ) public returns (bool success) {
+    require(listIds[_listId] == true, 'List does not exist');
+
     // verify ring signature
     require(UAOSRing.verify(
-      _pubkeys,
+      listIdToPubKeys[_listId],
       _tag,
       _tees,
       _seed,
       uint256(sha256(_listId))
     ), 'Invalid signature');
+
+    // check for duplicate identities
+    require(anonmyousIdentitiesMap[_anonymousId] == false, 'Identity has already been added');
 
     // verify tag uniqueness
     bytes memory tagBytes = abi.encodePacked(_tag);
@@ -76,7 +85,13 @@ contract AnonymousIdentityRegistry {
     // add entry to list
     address[] storage anonymousIds = listIdToAnonymousIds[_listId];
     anonymousIds.push(_anonymousId);
+    anonmyousIdentitiesMap[_anonymousId] = true;
+
     emit ListItemAdded(_listId, _anonymousId);
+
+    if (anonymousIds.length == 10) {
+      emit ListFilled(_listId);
+    }
     
     return true;
   }
--- truffle/test/AnonymousIdentityRegistry.js
+++ truffle/test/AnonymousIdentityRegistry.js
@@ -1,25 +1,40 @@
 'use strict'
-const { assert, expect } = require('./utils/testHelper.js')
 
-// ring signature for message '0xd3fd354067184687956bc8618a26e335'
-const signature = require('./data/signature.json')
+const { assert, expect } = require('./utils/testHelper.js')
 const AnonymousIdentityRegistry = artifacts.require('./AnonymousIdentityRegistry.sol')
+
+const listId = '0xd3fd354067184687956bc8618a26e335' // message being signed
+const signature = require('./data/signature_0.json') // valid signature for the above listId
+const signatureDiffMsg = require('./data/signature_0_hello.json') // valid signature for string 'hello'
 
 const flat = arr => arr.reduce((previous, current) => previous.concat(current), [])
 
+const expectErrorReason = async (fn, errorReason) => {
+  try {
+    await fn()
+  } catch (err) {
+    return expect(err.reason).to.equal(errorReason)
+  }
+  assert.fail('Expecting an error but none was caught')
+}
+
 contract('AnonymousIdentityRegistry', accounts => {
   const owner = accounts[0]
-  const listId = '0xd3fd354067184687956bc8618a26e335'
-  const entry = '0x9b7B86FC70bA2aD53e98d5F8F852c3629F813c7a'
   let contract
 
   before(async () => {
     contract = await AnonymousIdentityRegistry.deployed()
   })
 
-  it('should create new list', async () => {
+  it('should not allow anyone other than owner to create new list', async () => {
+    expectErrorReason(
+      () => contract.createList(listId, flat(signature.pkeys), { from: accounts[1] }),
+      'Unauthorized operation'
+    )
+  })
+
+  it('should allow contract owner to create new list', async () => {
     const { logs } = await contract.createList(listId, flat(signature.pkeys))
-    
     const { event, args } = logs[0]
     expect(logs.length).to.equal(1)
     expect(event).to.equal('ListCreated')
@@ -27,38 +42,49 @@ contract('AnonymousIdentityRegistry', accounts => {
   })
 
   it('should reject adding a duplicate list', async () => {
-    try {
-      await contract.createList(listId, flat(signature.pkeys))
-    } catch (err) {
-      return expect(err.reason).to.equal('List already exists')
-    }
-    assert.fail('Expecting an error but none was caught')
+    expectErrorReason(
+      () => contract.createList(listId, flat(signature.pkeys)),
+      'List already exists'
+    )
   })
 
-  it('should reject new entry to list when invalid signature provided', async () => {
-    const { pkeys, tag, tees, seed } = signature
+  it('should reject new entry to list when an invalid signature is provided', async () => {
+    const { tag, tees, seed } = signatureDiffMsg
 
-    try {
-      await contract.addToList(
-        'unsigned message',
-        entry,
-        flat(pkeys),
+    expectErrorReason(
+      () => contract.addToList(
+        listId,
+        accounts[0],
         tag,
         tees,
         seed
-      )
-    } catch (err) {
-      return expect(err.reason).to.equal('Invalid signature')
-    }
-    assert.fail('Expecting an error but none was caught')
+      ),
+      'Invalid signature'
+    )
   })
 
-  it('should accept new entry to list when valid signature provided', async () => {
-    const { pkeys, tag, tees, seed } = signature
+  it('should reject new entry when list does not exist', async () => {
+    const { tag, tees, seed } = signatureDiffMsg
+    const entry = accounts[0]
+
+    expectErrorReason(
+      () => contract.addToList(
+        'hello',
+        entry,
+        tag,
+        tees,
+        seed
+      ),
+      'List does not exist'
+    )
+  })
+
+  it('should accept new entry to list when a valid signature is provided', async () => {
+    const { tag, tees, seed } = signature
+    const entry = accounts[0]
     const { logs } = await contract.addToList(
       listId,
       entry,
-      flat(pkeys),
       tag,
       tees,
       seed
@@ -71,33 +97,72 @@ contract('AnonymousIdentityRegistry', accounts => {
     expect(args.anonymousId).to.equal(entry)
   })
 
-  it('should reject new entry to list when a duplicate signature is provided', async () => {
-    const { pkeys, tag, tees, seed } = signature
+  it('should reject duplicate entry to list by a different signer', async () => {
+    // different signature, duplicate entry
+    const { tag, tees, seed } = require('./data/signature_1.json')
+    const entry = accounts[0]
 
-    try {
-      await contract.addToList(
+    expectErrorReason(
+      () => contract.addToList(
         listId,
-        '0x6d480772b57e91f1c4e1cc196df88896d27ed327',
-        flat(pkeys),
+        entry,
+        tag,
+        tees,
+        seed
+      ),
+      'Identity has already been added'
+    )
+  })
+
+  it('should reject new entry to list when a duplicate signature is provided', async () => {
+    // duplicate signature, different entry
+    const { tag, tees, seed } = signature
+
+    expectErrorReason(
+      () => contract.addToList(
+        listId,
+        accounts[1],
+        tag,
+        tees,
+        seed
+      ),
+      'Duplicate signature'
+    )
+  })
+
+  it('should emit a ListFilled event when all slots are filled', async () => {
+    let lastResponse
+
+    for (let i = 1; i < 10; i++) {
+      const sig = require(`./data/signature_${i}.json`)
+
+      const { tag, tees, seed } = sig
+      lastResponse = contract.addToList(
+        listId,
+        accounts[i],
         tag,
         tees,
         seed
       )
-    } catch (err) {
-      return expect(err.reason).to.equal('Duplicate signature')
     }
-    assert.fail('Expecting an error but none was caught')
+
+    const { logs } = await lastResponse
+    const listFilledEvent = logs.find(log => log.event === 'ListFilled')
+    expect(listFilledEvent).not.to.be.undefined
+    expect(listFilledEvent.args.listId).to.equal(listId)
   })
 
   it('should return all entries for the list', async () => {
     const result = await contract.getList(listId)
-    expect(result).to.deep.equal([ entry ])
+    expect(result.length).to.be.equal(10)
+    expect(result).to.have.members(accounts)
   })
 
-  // TODO loop - do the same thing for all identities in the pkeys list, fill up the list
+  // TODO remove hard coded array size
   // TODO add initial hash transaction
+
   it.skip('should reject a hash when an invalid non-linkable ring signature is provided', () => {
-    
+
   })
 
   it.skip('should accept a hash when a valid non-linkable ring signature is provided', () => {
