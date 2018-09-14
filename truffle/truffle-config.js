eeca4323ee3dc22d3ed064e807c4254a9819a303
Jimmy Chen <jchen.tc@gmail.com>
2018-09-14 15:12:40 +0100

    Add eth-gas-reporter

--- truffle/truffle-config.js
+++ truffle/truffle-config.js
@@ -12,5 +12,11 @@ module.exports = {
     solc: {
       version: '0.4.24'
     }
+  },
+  mocha: {
+    reporter: 'eth-gas-reporter',
+    reporterOptions : {
+      showTimeSpent: true
+    }
   }
 }
